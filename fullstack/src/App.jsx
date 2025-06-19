2f1cbd5368f07e3b8681686a009b712ee84df4ad
Suriyaprakash-dot <suryaspidy7@gmail.com>
2025-06-19 16:17:51 +0530

    day3

---
+++ fullstack/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Welcome from './Welcome'
+
+//import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Welcome/>
+      <h1>suriya</h1>
+      </>
+  )
+}
+
+export default App
