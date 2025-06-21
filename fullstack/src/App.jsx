9e86d311cdf57f3da3ee801c48b84804feb4fb04
Suriyaprakash-dot <suryaspidy7@gmail.com>
2025-06-21 20:41:43 +0530

    DAY5

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import Welcome from './Welcome';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Services from './Pages/Services';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Welcome from './Welcome'
+import Skills from './skills'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Services from './Pages/Services'
+import { Route,Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+//import './App.css'
 
-// HOOK components
-import Form from './hooks/Form';
-import Effect from './hooks/Effect';
-import Reducer from './hooks/Reducer';
-import State from './hooks/State';
+import Form from './hooks/Form'
+import Effect from './hooks/Effect'
+import Reducer from './hooks/Reducer'
+import State from './hooks/state'
 
 function App() {
-  return (
+ 
+
+return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/state' element={<State />} />
-        <Route path='/form' element={<Form />} />
-        <Route path='/effect' element={<Effect />} />
-        <Route path='/reducer' element={<Reducer />} />
-      </Routes>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/services' element={<Services/>}/>
+      <Route path='/State' element={<State/>}/>
+      <Route path='/Form' element={<Form/>}/>
+      <Route path='/Effect' element={<Effect/>}/>
+      <Route path='/Reducer' element={<Reducer/>}/>
+    </Routes>
+    
+        
     </>
-  );
+  )
 }
 
-export default App;
+export default App
